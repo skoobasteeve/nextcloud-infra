30d45d6474cf58be8b4f11d8ef41ca2c9fb29509
Ray Lyon <ray@raylyon.net>
2022-10-02 19:16:07 -0400

    create dirs for containers

--- packer/image_conf.sh
+++ packer/image_conf.sh
@@ -5,6 +5,10 @@ echo "$PASSWORD" | passwd --stdin "$USERNAME"
 
 usermod -aG wheel "$USERNAME"
 mkdir -p "$PODMAN_DIR/nextcloud"
+mkdir -p "$PODMAN_DIR/nextcloud/nextcloud-config"
+mkdir -p "$PODMAN_DIR/nextcloud/mariadb"
+mkdir -p "$PODMAN_DIR/nextcloud/caddy"
+mkdir -p "$PODMAN_DIR/nextcloud/caddy-data"
 mkdir -p "$SYSTEMD_DIR"
 
 dnf update -y
