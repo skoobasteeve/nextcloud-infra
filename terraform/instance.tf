437e74bc3043820b31d681f2d9887b43cb3925e7
Ray Lyon <ray@raylyon.net>
2022-10-02 11:24:47 -0400

    use latest packer image

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,10 +1,19 @@
+
+data "linode_images" "nextcloud_image" {
+  latest = true
+  filter {
+    name = "label"
+    values = [var.IMAGE_LABEL]
+  }
+}
+
 resource "linode_sshkey" "local" {
   label = "local"
   ssh_key = chomp(file("~/.ssh/id_rsa.pub"))
 }
 
 resource "linode_instance" "nextcloud" {
-        image = "linode/centos-stream9"
+        image = data.linode_images.nextcloud_image.images[0].id
         label = "Nextcloud"
         group = "Terraform"
         region = "us-east"
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -7,3 +7,8 @@ variable "ROOT_PASS" {
   type = string
   sensitive = true
 }
+
+variable "IMAGE_LABEL" {
+  type = string
+  default = "packer-centos-stream-9"
+}
