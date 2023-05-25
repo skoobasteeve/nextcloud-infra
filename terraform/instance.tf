02ba48dc6d434cea7c113c5b8135db253e99fd7f
Ray Lyon <ray@raylyon.net>
2023-05-25 13:11:23 -0400

    dedicated ipv4

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -3,15 +3,24 @@ resource "hcloud_ssh_key" "local" {
   public_key = file(var.SSH_KEYFILE)
 }
 
+resource "hcloud_primary_ip" "nextcloud" {
+  name = "nextcloud"
+  type = "ipv4"
+  datacenter = "ash-dc1"
+  auto_delete = false
+  assignee_type = "server"
+}
+
 resource "hcloud_server" "nextcloud" {
   name = "Nextcloud"
   image = "centos-stream-9"
   server_type = "cpx11"
-  location = "ash"
+  datacenter = "ash-dc1"
   ssh_keys = [hcloud_ssh_key.local.id]
   firewall_ids = [hcloud_firewall.nextcloud.id]
   public_net {
     ipv4_enabled = true
+    ipv4 = hcloud_primary_ip.nextcloud.id
     ipv6_enabled = true
   }
 }
