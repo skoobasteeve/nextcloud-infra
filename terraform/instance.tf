79b7315a9c5949b26d1068804009b7eb7469addf
Ray Lyon <ray@raylyon.net>
2023-06-30 14:57:49 -0400

    alt ssh port in cloud-config

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,6 +1,9 @@
 resource "hcloud_ssh_key" "local" {
   name       = "local"
   public_key = file(var.SSH_KEYFILE)
+  lifecycle {
+    ignore_changes = [public_key]
+  }
 }
 
 resource "hcloud_primary_ip" "nextcloud" {
@@ -16,13 +19,16 @@ resource "hcloud_server" "nextcloud" {
   image        = "centos-stream-9"
   server_type  = "cpx11"
   datacenter   = "ash-dc1"
-  user_data    = templatefile("${path.module}/cloud-config.yml.tftpl", { user = var.USER, ssh_key = file(var.SSH_KEYFILE), pass_hash = var.PASS_HASH })
+  user_data    = templatefile("${path.module}/cloud-config.yml.tftpl", { user = var.USER, ssh_key = file(var.SSH_KEYFILE), pass_hash = var.PASS_HASH, ssh_port = tonumber(var.ALT_SSH_PORT) })
   ssh_keys     = [hcloud_ssh_key.local.id]
   firewall_ids = [hcloud_firewall.nextcloud.id]
   public_net {
     ipv4_enabled = true
     ipv4         = hcloud_primary_ip.nextcloud.id
     ipv6_enabled = true
+  }
+  lifecycle {
+    ignore_changes = [user_data]
   }
 }
 
