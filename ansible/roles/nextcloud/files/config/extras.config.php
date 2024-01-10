c1b9c845003dff1a079737e77b94720d4a15d72e
Ray Lyon <ray@raylyon.net>
2024-01-10 16:40:50 -0500

    nextcloud additional configuration

---
+++ ansible/roles/nextcloud/files/config/extras.config.php
@@ -0,0 +1,11 @@
+<?php
+
+$CONFIG['trusted_proxies'] = array(0 => 'localhost',);
+$CONFIG['default_phone_region'] = 'US';
+$CONFIG['preview_max_x'] = '2048';
+$CONFIG['preview_max_y'] = '2048';
+$CONFIG['jpeg_quality'] = '60';
+
+
+
+
