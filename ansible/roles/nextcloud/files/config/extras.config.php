e92f4978de81a3f1ffdea19b63bb5e3349c32ac3
Ray Lyon <ray@raylyon.net>
2024-04-28 19:36:10 -0400

    use ip instead of hostname for trusted_proxies

--- ansible/roles/nextcloud/files/config/extras.config.php
+++ ansible/roles/nextcloud/files/config/extras.config.php
@@ -1,6 +1,6 @@
 <?php
 
-$CONFIG['trusted_proxies'] = array(0 => 'localhost',);
+$CONFIG['trusted_proxies'] = array(0 => '127.0.0.1',);
 $CONFIG['default_phone_region'] = 'US';
 $CONFIG['preview_max_x'] = '2048';
 $CONFIG['preview_max_y'] = '2048';
